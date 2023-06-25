529d4c89c38838f7d928a4b8cce37259cd9a2148
Mathias Poimer <wi22b114@technikum-wien.at>
2023-06-25 12:22:32 +0200

    Project setup

---
+++ src/test/java/com/poimer/exampoimer/ExamPoimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poimer.exampoimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPoimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
